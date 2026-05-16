fb04313f7f4e635a0443127c10fdfd411c19f269
Juman <jumanm123@gmail.com>
2026-05-16 19:00:20 +0300

    Implement path selection for light presets
    Added logic to determine the path based on selected preset.

--- LightingActivity_code_snippet.java
+++ LightingActivity_code_snippet.java
@@ -1,9 +1,17 @@
 import com.android.volley.toolbox.Volley;
-private static final String PREFS_NAME = "InABlinkPrefs";
+
 private static final String ESP_IP = "http://172.20.10.3"; //ip address is changed based on connection
 
 
 //code inside calling method setLight
+String path;
+        if (selected.equals(presetOff)){
+            path = "/off";
+        } else if (selected.equals(presetDim)){
+            path = "/dim";
+        } else{
+            path = "/on";
+        }
 RequestQueue queue = Volley.newRequestQueue(this);
         String url = ESP_IP + path;
 
